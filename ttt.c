28befc236bf54866a3289089c0a0bc48e0b39712
AlandisAyupov <100100917+AlandisAyupov@users.noreply.github.com>
2023-04-11 13:19:02 -0400

    Update ttt.c

--- ttt.c
+++ ttt.c
@@ -13,70 +13,98 @@
 volatile int active = 1;
 int connectSock(char *host, char *service)
 {
- struct addrinfo hints, *info_list, *info;
- int sock, error;
- memset(&hints, 0, sizeof(hints));
- hints.ai_family = AF_UNSPEC;
- hints.ai_socktype = SOCK_STREAM;
- error = getaddrinfo(host, service, &hints, &info_list);
- if (error)
- {
-  fprintf(stderr, "Error looking up %s:%s: %s\n", host, service, gai_strerror(error));
-  return -1;
- }
- for (info = info_list; info != NULL; info = info->ai_next)
- {
-  sock = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
-  if (sock < 0)
-   continue;
-  error = connect(sock, info->ai_addr, info->ai_addrlen);
+  struct addrinfo hints, *info_list, *info;
+  int sock, error;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  error = getaddrinfo(host, service, &hints, &info_list);
   if (error)
   {
-   close(sock);
-   continue;
+    fprintf(stderr, "Error looking up %s:%s: %s\n", host, service, gai_strerror(error));
+    return -1;
   }
-  break;
- }
- freeaddrinfo(info_list);
- if (info == NULL)
- {
-  fprintf(stderr, "Unable to connect to %s:%s\n", host, service);
-  return -1;
- }
- return sock; 
+  for (info = info_list; info != NULL; info = info->ai_next)
+  {
+    sock = socket(info->ai_family, info->ai_socktype, info->ai_protocol);
+    if (sock < 0)
+      continue;
+    error = connect(sock, info->ai_addr, info->ai_addrlen);
+    if (error)
+    {
+      close(sock);
+      continue;
+    }
+    break;
+  }
+  freeaddrinfo(info_list);
+  if (info == NULL)
+  {
+    fprintf(stderr, "Unable to connect to %s:%s\n", host, service);
+    return -1;
+  }
+  return sock;
+}
+char side(char *buf)
+{
+  int i = 0;
+  while(buf[i] != '|')
+    i++;
+  i++;
+  while(buf[i] != '|')
+    i++;
+  i++;
+  return buf[i];
 }
 int cond(char *buf)
 {
- if(buf[0] == 'W' && buf[1] == 'A' && buf[2] == 'I' && buf[3] == 'T')
-  return 1;
- if(buf[0] == 'M' && buf[1] == 'O' && buf[2] == 'V' && buf[3] == 'D')
-  return 1;
- return 0;
+  if (buf[0] == 'W' && buf[1] == 'A' && buf[2] == 'I' && buf[3] == 'T')
+    return 2;
+  if (buf[0] == 'M' && buf[1] == 'O' && buf[2] == 'V' && buf[3] == 'D')
+    return 1;
+  return 0;
 }
 int main(int argc, char **argv)
 {
- int sock, bytes;
- char buf[BUFLEN];
- if (argc != 3)
- {
-  printf("Host/Servive Required\n");
-  exit(EXIT_FAILURE);
- }
- sock = connectSock(argv[1], argv[2]);
- if (sock < 0)
-  exit(EXIT_FAILURE);
- while ((bytes = read(STDIN_FILENO, buf, BUFLEN)) > 0)
- {
-  buf[bytes] = '\0';
-  write(sock, buf, bytes);
+  int sock, bytes;
+  char buf[BUFLEN];
+  if (argc != 3)
+  {
+    printf("Host/Servive Required\n");
+    exit(EXIT_FAILURE);
+  }
+  sock = connectSock(argv[1], argv[2]);
+  if (sock < 0)
+    exit(EXIT_FAILURE);
+  while ((bytes = read(STDIN_FILENO, buf, BUFLEN)) > 0)
+  {
+    buf[bytes] = '\0';
+    write(sock, buf, bytes);
+    bytes = read(sock, buf, BUFLEN);
+    write(STDOUT_FILENO, buf, bytes);
+    if (cond(buf) == 2)
+      break;
+  }
   bytes = read(sock, buf, BUFLEN);
   write(STDOUT_FILENO, buf, bytes);
-  if(cond(buf))
+  char s = side(buf);
+  if (s == 'O')
   {
-   bytes = read(sock, buf, BUFLEN);
-   write(STDOUT_FILENO, buf, bytes);
+    bytes = read(sock, buf, BUFLEN);
+    write(STDOUT_FILENO, buf, bytes);
   }
- }
- close(sock);
- return EXIT_SUCCESS;
+  while ((bytes = read(STDIN_FILENO, buf, BUFLEN)) > 0)
+  {
+    buf[bytes] = '\0';
+    write(sock, buf, bytes);
+    bytes = read(sock, buf, BUFLEN);
+    write(STDOUT_FILENO, buf, bytes);
+    if (cond(buf) == 1)
+    {
+      bytes = read(sock, buf, BUFLEN);
+      write(STDOUT_FILENO, buf, bytes);
+    }
+  }
+  close(sock);
+  return EXIT_SUCCESS;
 }
